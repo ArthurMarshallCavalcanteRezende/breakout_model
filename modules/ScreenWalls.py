c7cd99fed5cf5132546f10c8d30e916f97878478
Arthur <amcr.snf24@uea.edu.br>
2024-12-02 18:29:19 -0400

    x and y position added for screen walls.

--- modules/ScreenWalls.py
+++ modules/ScreenWalls.py
@@ -2,14 +2,18 @@ class ScreenWalls:
     def __init__(self):
         self.width = 1
         self.height = 700
-        self.x = 1
-        self.y = 5
+        self.x = 0
+        self.y = 0
 
     def draw(self, position):
         if position == "LEFT":
             print("Left wall created!")
+            self.x = 1
         elif position == "RIGHT":
             print("Right wall created!")
+            self.x = 700
         elif position == "UP":
             print("Top border created!")
+            self.x = 350
+            self.y = 700
 
